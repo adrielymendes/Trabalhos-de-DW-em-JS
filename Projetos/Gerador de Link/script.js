432a970b37c47b1a17bc3b40c756317d412b660e
Hellenn3 <adrielymen@gmail.com>
2024-05-03 09:59:20 -0300

    script

---
+++ Projetos/Gerador de Link/script.js
@@ -0,0 +1,6 @@
+const input= document.getElementById("[input]");
+const btnlink = document.querySelector("[gerar-link]");
+const btnSend = document.querySelector("[copiar-link]");
+//const linkGerado = document.createElement("input");
+//const btnCopiarLink = document.createElement("button");
+
